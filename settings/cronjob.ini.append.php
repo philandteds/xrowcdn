70133770804f6db535adebfe057be5a69a6d6e8c
Geoff Bentley <geoff@philandteds.com>
2016-01-12 08:50:19 +1300

    turn off cronjobs

--- settings/cronjob.ini.append.php
+++ settings/cronjob.ini.append.php
@@ -1,12 +1,12 @@
 <?php /* #?ini charset="utf-8"?
 
 [CronjobSettings]
-Scripts[]=xrow_cdn_upload.php
-Scripts[]=upload_compressed_jscss.php
+#Scripts[]=xrow_cdn_upload.php
+#Scripts[]=upload_compressed_jscss.php
 ExtensionDirectories[]=xrowcdn
 
 [CronjobPart-xrowcdn]
-Scripts[]=xrow_cdn_upload.php
-Scripts[]=upload_compressed_jscss.php
+#Scripts[]=xrow_cdn_upload.php
+#Scripts[]=upload_compressed_jscss.php
 
 */ ?>
